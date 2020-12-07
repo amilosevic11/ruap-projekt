1d2e0a746970717a8e661c166a2d50ef637e2098
amilosevic11 <fletchko.am@gmail.com>
2020-12-07 13:19:42 +0100

    commit

---
+++ application.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_RUAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Antonija Miloševića.");
+        }
+    }
+}
